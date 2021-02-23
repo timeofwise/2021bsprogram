d8a3277ccbe3001ae007ca13511fea19fb591e08
timeofwise <shockingpark@gmail.com>
2021-02-23 11:28:49 +0900

    modified Survey admin

--- survey/admin.py
+++ survey/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import Survey
 # Register your models here.
 
-admin.site.register(Survey)
+# admin.site.register(Survey)
+class SurveyAdmin(admin.ModelAdmin):
+    list_display = ['id', 'company', 'name', 'q2_overall', 'q3_3_recommend_to_other']
+    # raw_id_fields = ['author']
+    list_filter = ['company']
+    search_fields = ['company']
+    ordering = ['-created']
+
+admin.site.register(Survey,SurveyAdmin)
