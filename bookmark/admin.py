3e4ace51a6cd2909bd41744ca90a497ad9e1c98c
timeofwise <shockingpark@gmail.com>
2021-02-23 12:26:01 +0900

    modified Survey/Bookmark admin

--- bookmark/admin.py
+++ bookmark/admin.py
@@ -9,9 +9,9 @@ admin.site.register(Machine)
 # Register your models here.
 from .models import *
 class BookmarkAdmin(admin.ModelAdmin):
-    list_display = ['id', 'client', 'created', 'updated']
+    list_display = ['id', 'client', 'model_type', 'model_no', 'created', 'updated']
     # raw_id_fields = ['author']
-    list_filter = ['created', 'updated', 'client']
+    list_filter = ['client', 'created', 'updated']
     search_fields = ['client', 'created', 'model_type']
     ordering = ['-updated', '-created']
 
