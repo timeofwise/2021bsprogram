cf17c26d295e76317ce7fbb85db3b8d43f690293
timeofwise <shockingpark@gmail.com>
2021-02-23 10:05:33 +0900

    modified bookmark admin

--- bookmark/admin.py
+++ bookmark/admin.py
@@ -2,6 +2,17 @@ from django.contrib import admin
 from .models import Bookmark, Machine, Client
 # Register your models here.
 
-admin.site.register(Bookmark)
+# admin.site.register(Bookmark)
 admin.site.register(Client)
 admin.site.register(Machine)
+
+# Register your models here.
+from .models import *
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ['id', 'client', 'created', 'updated']
+    # raw_id_fields = ['author']
+    list_filter = ['created', 'updated', 'client']
+    search_fields = ['client', 'created', 'model_type']
+    ordering = ['-updated', '-created']
+
+admin.site.register(Bookmark,BookmarkAdmin)
