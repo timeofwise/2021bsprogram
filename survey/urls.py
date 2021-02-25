545251765d743a16c5e2163c684ee6e9ceab3020
timeofwise <shockingpark@gmail.com>
2021-02-25 11:03:43 +0900

    survey result view added

--- survey/urls.py
+++ survey/urls.py
@@ -4,6 +4,6 @@ from .views import *
 #app_name = 'survey'
 urlpatterns = [
     path('', AddSurvey.as_view(), name='survey_add'),
-    path('result/', DisplaySurvey.as_view(), name='survey_result'),
+    path('result/', DisplaySurvey.as_view(), name='survey_list'),
     path('complete/', SurveyComplete, name='survey_complete'),
 ]
