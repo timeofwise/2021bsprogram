ebd6a3651a8831ee5db8ba12caaa229b4adeb322
timeofwise <shockingpark@gmail.com>
2021-02-27 13:56:59 +0900

    consumable relations 1

--- parts/urls.py
+++ parts/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', PartsListView, name='parts'),
+    path('<slug:client_slug>/', PartsListView, name='parts_by_client'),
 ]
--- parts/views.py
+++ parts/views.py
@@ -1,19 +1,21 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.list import ListView
 # from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
-from bookmark.models import Bookmark, Consumed, Machine
+from bookmark.models import Bookmark, Consumed, Machine, Client
 from django.urls import reverse_lazy
 
-def PartsListView(request):
-    # 보여줄 사진 데이터
+def PartsListView(request, client_slug=None):
+    current_client = None
+    clients = Client.objects.all()
     machines = Machine.objects.all()
     # lists = Bookmark.objects.all()
     lists = Bookmark.objects.filter(available_display=True)
     parts = Consumed.objects.all()
-    sm421 = 'SM421'
-    sm411 = 'SM411'
-    decan_f2 = 'DECAN F2'
+
+    if client_slug:
+        current_client = get_object_or_404(Client, slug=client_slug)
+        lists = lists.filter(client=current_client)
 
 
-    return render(request, 'parts/parts.html', {'lists' : lists, 'parts' : parts, 'machines' : machines, 'sm421' : sm421, 'sm411' : sm411})
+    return render(request, 'parts/parts.html', {'lists' : lists, 'parts' : parts, 'machines' : machines, 'clients' : clients, 'current_client' : current_client})
