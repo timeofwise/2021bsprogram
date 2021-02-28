bb91e567a43f3e208c406f9c75ed02d6c7f1ad82
timeofwise <shockingpark@gmail.com>
2021-02-28 21:21:18 +0900

    add models for reports 9

--- config/settings.py
+++ config/settings.py
@@ -123,7 +123,7 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
