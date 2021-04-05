935649f217d33dadb3c5ab063f50540dd26be264
timeofwise <shockingpark@gmail.com>
2021-04-05 13:57:36 +0900

    double-navbar

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('software/t-solution/', software_t_solution, name="t-solution"),
+    path('t-solution/', software_t_solution, name="t-solution"),
 ]
